032c897ad02feba155425fc5931b1df7a77248a2
tirthbhavsar-code <tirth.bhavsar@binarypenguin.co>
2026-02-10 12:09:43 +0530

    Added multiple comment use code

---
+++ fullcomment.py
@@ -0,0 +1,19 @@
+# This program comment use
+
+print("Python scripting start")
+
+# variable
+name = "Tirth"
+print("Name:", name)
+
+'''
+This is multi line comment
+for explanation purpose
+'''
+
+# math
+a = 10
+b = 5
+print("Addition:", a+b)
+
+print("Program end")
