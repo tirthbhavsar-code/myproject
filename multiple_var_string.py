df8f92725642e0f02430c3b5a1bf7c3d6aca6ca1
tirthbhavsar-code <tirth.bhavsar@binarypenguin.co>
2026-02-10 16:25:13 +0530

    Working with Multiple Variables & Strings

---
+++ multiple_var_string.py
@@ -0,0 +1,10 @@
+company = "Google"
+employee = "Tirth"
+salary = 50000
+
+print("Company:", company)
+print("Employee:", employee)
+print("Salary:", salary)
+
+report = f"{employee} works at {company} and salary is {salary}"
+print(report)
