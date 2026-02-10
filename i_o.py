a553f6da9c586dda833a5182f48b94ee4eb2c699
tirthbhavsar-code <tirth.bhavsar@binarypenguin.co>
2026-02-10 17:26:59 +0530

    Full i/o code added

--- i_o.py
+++ i_o.py
@@ -4,3 +4,15 @@ amount = int(input("Enter payment: "))
 print("Client:", name)
 print("Payment:", amount)
 print("Thank you")
+
+
+
+
+name = input("Enter name: ")
+city = input("Enter city: ")
+age = int(input("Enter age: "))
+
+print("\n--- OUTPUT ---")
+print("Name:", name)
+print("City:", city)
+print("Age:", age)
