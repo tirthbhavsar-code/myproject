4b82ac998260334c347c453b907e9c588f033116
tirthbhavsar-code <tirth.bhavsar@binarypenguin.co>
2026-02-11 10:32:42 +0530

    comment added

--- allstring.py
+++ allstring.py
@@ -1,23 +1,23 @@
 text = "python scripting"
 
-# 1. Upper & lower
+# 1. Upper & lower convert krva
 print("\nUpper:", text.upper())
 print("Lower:", text.lower())
 
-# 2. Title & capitalize
+# 2. Title & capitalize krva
 print("Title:", text.title())
 print("Capitalize:", text.capitalize())
 
-# 3. Length
+# 3. Length count krva
 print("Length:", len(text))
 
-# 4. Replace
+# 4. Replace krva mate
 print("Replace:", text.replace("python", "java"))
 
-# 5. Find position
+# 5. Find position mate
 print("Find:", text.find("script"))
 
-# 6. Count word
+# 6. Count word ketla word che string ma check krva
 print("Count:", text.count("p"))
 
 # 7. Check start & end
@@ -31,11 +31,11 @@ b = "12345"
 print("\nIs alpha:", a.isalpha())
 print("Is digit:", b.isdigit())
 
-# 9. Remove space
+# 9. Remove space krva
 c = "   hello BinaryPenguin   "
 print("Strip:", c.strip())
 
-# 10. Split string
+# 10. Split string mate
 d = "apple,banana,mango"
 print("Split:", d.split(","))
 
@@ -43,7 +43,7 @@ print("Split:", d.split(","))
 list1 = ["Python", "is", "easy"]
 print("Join:", " ".join(list1))
 
-# 12. Index access
+# 12. Index access krva
 name = "Python"
 print("\nFirst letter:", name[0])
 print("Last letter:", name[-1])
